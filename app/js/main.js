c567015b4b6b9111eebee931c2b7f58414c93850
lalit sachdeva <sachdeva_libra@yahoo.com>
2015-06-10 01:27:09 +0530

    updated for partials mijnolympua administratie to hide some menus

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,33 @@
+//to toggle the accordion
 function toggleView(obj) // the li element clicked in the current scope
 {
     var element = obj;  // This is the DOM object being clicked
     var $this = $(obj); // This is the jQuery object being clicked
-    $this.closest('li').toggleClass('show');
+    //this is to remove the previous show class from the ul li elements
+    console.log($this.closest('li').hasClass('show'));
+    //this will add new show class where user has clicked
+    
+    if($this.closest('li').hasClass('show')==true){
+        $('body .accordion-data > li').removeClass('show');
+    }else{
+        $('body .accordion-data > li').removeClass('show');
+        $this.closest('li').addClass('show');   
+    }
+    
+    //this will update the caret icon based on the accordion is opened or closed
     var $switchClass = $this.find('.olympia-icon');
-    if($switchClass.hasClass('olympia-caret-down')){
+    if($this.closest('li').hasClass('show')){
         $switchClass.removeClass('olympia-caret-down');
         $switchClass.addClass('olympia-caret-up');
     }else{
         $switchClass.addClass('olympia-caret-down');
         $switchClass.removeClass('olympia-caret-up');
     }
+    //this will scroll the page to the accordion where user has clicked
+    $this.scrollView();
 }
 
+//to work the tooltip
 $('#tooltip1,#tooltip2,#tooltip3,#tooltip4,#tooltip5,#tooltip6,#tooltip7,#tooltip8,#tooltip9,#tooltip10,#tooltip11,#tooltip12,#tooltip13,#tooltip14,#tooltip15,#tooltip16,#tooltip17,#tooltip18,#tooltip19,#tooltip20,#tooltip21').popover({
     container: 'body',
     html: true,
@@ -20,12 +35,20 @@ $('#tooltip1,#tooltip2,#tooltip3,#tooltip4,#tooltip5,#tooltip6,#tooltip7,#toolti
 })
 .css({'cursor': 'pointer'});
 
+//to hide or toggle the popover on clicking anywhere on the body
 $('body').on('click', function (e) {
     $('[data-toggle="popover"]').each(function () {
-        //the 'is' for buttons that trigger popups
-        //the 'has' for icons within a button that triggers a popup
         if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
             $(this).popover('hide');
         }
     });
 });
+
+//to scroll to a point on the screen where user clicked
+$.fn.scrollView = function () {
+    return this.each(function () {
+        $('html, body').animate({
+            scrollTop: $(this).offset().top -10
+        }, 1000);
+    });
+}
